3d3cdeaf0f2867c9e787c65823f6900426e156a8
raracalista <162337745+raracalista@users.noreply.github.com>
2024-06-06 14:34:38 +0700

    Create soal1.c

---
+++ soal1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+typedef struct KHS *address;
+    struct KHS{
+     int Nim;
+     address IP;
+};
+    
+void updateIP(int Nimnya){
+address perubahan=new KHS;
+}
+
+void printKHS(){
+
+}
+
+int main(){
+    NIM(24052);
+    IP(3.85);
+}
